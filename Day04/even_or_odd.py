73f68afbd59a9319182ff37082f21fca6ba6a6b6
Pranjal <pranjallolenglish@gmail.com>
2026-04-11 13:29:37 +0545

    Add even or odd checker in even_or_odd.py
    Implement a program to check if a number is even or odd.

---
+++ Day04/even_or_odd.py
@@ -0,0 +1,9 @@
+n = int(input("Enter the Number"))
+
+// input the number to check the number is odd or even
+
+if n%2==0:  // Conditional Statements to verify the number is odd if not then else show its even. 
+  print("even")
+else:
+  print("odd")
+
