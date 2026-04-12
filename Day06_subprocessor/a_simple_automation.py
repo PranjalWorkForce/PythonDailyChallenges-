7c9d29cdb70198d4cf3a74a6913a7edfafb6b90d
Pranjal <pranjallolenglish@gmail.com>
2026-04-12 12:40:43 +0545

    Change command to echo 'Hello World' and print result
    Updated the script to run 'echo Hello World' and print the output.

--- Day06_subprocessor/a_simple_automation.py
+++ Day06_subprocessor/a_simple_automation.py
@@ -1,5 +1,10 @@
 import subprocess
 
-cmd = subprocess.run(["ls","-l"], capture_output=True, text=True)
+# Run the command and capture what it says
+result = subprocess.run(["echo", "Hello World"], capture_output=True, text=True)
+// capture_output = it store the output to use it in Python script. 
+// text = True so it store the data as string not as bytes. 
 
-print(cmd)
+# Now you can use that text in your code
+print(f"The program said: {result.stdout}") 
+
