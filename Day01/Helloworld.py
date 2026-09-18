143128d0565a12954d5db3d8b5de32c47f7794db
Pranjal <pranjallolenglish@gmail.com>
2026-09-18 21:34:26 +0545

    Add comments to Helloworld.py
    Added comments to explain code functionality.

--- Day01/Helloworld.py
+++ Day01/Helloworld.py
@@ -1,3 +1,6 @@
 print("HelloWorld") 
 
+
+// code is easy but not the life. learn hard work and practice hard too. 
+
 // print = display the text entered inside the braces () and string must be inside the double quoted "Text Here"
